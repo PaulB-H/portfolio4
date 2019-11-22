548c0cc336cacb7c0c9516ac0059d9cea87b46b6
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-11-22 13:19:49 -0500

    removed boostrap, styled nav links, added fade in / out events upon clicking nav link, added about text

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-// $("button").click(function () {
-//     $("h1, h2, p").toggleClass("blue");
-// });
+$(document).ready(function(){
+
+    function pageLoad () {
+        $("#head").fadeIn();
+        setTimeout(function(){ 
+            $("#content").fadeIn(); 
+        }, 500);
+    }
+
+    pageLoad();
+
+    $(".aboutLink").click(function () {
+        $(".aboutLink").addClass("active");
+        $(".portfolioLink").removeClass("active");
+        $(".contactLink").removeClass("active");
+        $("#portfolio").fadeOut();
+        $("#contact").fadeOut();
+        setTimeout(function(){ 
+            $("#about").fadeIn(); 
+        }, 500);
+    });
+
+
+    $(".portfolioLink").click(function () {
+        $(".portfolioLink").addClass("active");
+        $(".aboutLink").removeClass("active");
+        $(".contactLink").removeClass("active");
+        $("#about").fadeOut();
+        $("#contact").fadeOut();
+        setTimeout(function(){ 
+            $("#portfolio").fadeIn(); 
+        }, 500);
+    });
+
+
+
+    $(".contactLink").click(function () {
+        $(".contactLink").addClass("active");
+        $(".aboutLink").removeClass("active");
+        $(".portfolioLink").removeClass("active");
+        $("#about").fadeOut();
+        $("#portfolio").fadeOut();
+        setTimeout(function(){ 
+            $("#contact").fadeIn(); 
+        }, 500);
+    });
+
+
+
+});
+
+
