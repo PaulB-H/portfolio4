d8d8ae2e24d27629766f05ed72b67e68e69a4702
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-11-24 18:48:48 -0500

    Fixed link for toastr, added list of projects I will include on portfolio section

--- script.js
+++ script.js
@@ -43,8 +43,32 @@ $(document).ready(function(){
         }, 500);
     });
 
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
 
+    $("#copyToClipBtn").click(function(){
+        var copyText = document.querySelector("#copyToClipText");
+        copyText.select();
+        document.execCommand("copy");
+        toastr["success"]("Copied email to clipboard")
+    });
 
-
+ 
+    
 
 });
